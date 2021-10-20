e6cbe542643731359b02b9c3eb9cb7f7013a343e
Sue-52 <1219243947@qq.com>
2021-10-20 20:34:06 +0800

    submit：update

---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,9 @@
+const sidebar = [
+  {
+    text: "Mysql 简介",
+    collapsable: false,
+    children: ["/guide/00.Mysql简介/Mysql简介.md"],
+  },
+];
+
+module.exports = sidebar;
---
+++ docs/guide/00.Mysql简介/README.md
@@ -0,0 +1,8 @@
+---
+title: Mysql
+lang: en-US
+---
+
+# Mysql
+
+学习 Mysql 中的必会知识点。💛💚💙💜🤎🖤
